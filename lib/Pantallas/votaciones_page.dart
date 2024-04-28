86178ab0f99c665df9c0d77487db82bd657e1145
MelvinMajano <melvinmajano06@gmail.com>
2024-04-28 15:17:47 -0600

    Commit de retoque

--- lib/Pantallas/votaciones_page.dart
+++ lib/Pantallas/votaciones_page.dart
@@ -38,17 +38,32 @@ class VotacionesPage extends StatelessWidget {
                 itemCount: listabanda.length,
                 itemBuilder: (BuildContext context, int index) {
                   final banda = listabanda[index];
+                  final foto = banda['Portada'];
+                  ImageProvider imagenProvider = const AssetImage('assets/No Imagen.jpg');
                   return AnimationConfiguration.staggeredList(
                       position: index,
                       duration: const Duration(milliseconds: 375),
                       child: SlideAnimation(
                           child: FadeInAnimation(
                         child: GestureDetector(
-                          onTap: () {},
+                          onTap: () async {
+                            if (banda['Portada'] == "") {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text(
+                                          'No se puede votar por una banda sin portada')));
+                              return;
+                            }
+                            final votos = banda['votos'] + 1;
+                            await instance
+                                .collection('Bandas')
+                                .doc(banda.id)
+                                .update({'votos': votos});
+                          },
                           child: Container(
                             alignment: Alignment.center,
                             width: MediaQuery.of(context).size.width * 0.9,
-                            padding: const EdgeInsets.all(20),
+                            padding: const EdgeInsets.all(30),
                             margin: const EdgeInsets.only(
                                 top: 20, left: 20, right: 20),
                             decoration: BoxDecoration(
@@ -56,28 +71,57 @@ class VotacionesPage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(10),
                             ),
                             child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(15.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                        image: NetworkImage(banda["Portada"]),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                    height: 100,
-                                    width: 100,
-                                  ),
-                                ),
                                 Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     Text(
                                       banda["nombre"],
+                                      style: const TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(height: 4),
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(15.0),
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                            image: foto != ""
+                                                ? NetworkImage(foto)
+                                                : imagenProvider,
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                        height: 100,
+                                        width: 100,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      banda["album"],
+                                      style: const TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.bold),
                                     ),
                                     const SizedBox(height: 10),
                                     Text(
                                       banda['anioLanzamiento'],
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          'Votos: ${banda['votos']}',
+                                        ),
+                                        const SizedBox(width: 3),
+                                        Icon(Icons.thumb_up, color: Colors.blue[900], size: 14,)
+                                      ],
                                     ),
                                   ],
                                 ),
